f2952d535de56a4c72c7d0163978a1232e5c5737
Majo Cortez <52331693+Majo-Cortez@users.noreply.github.com>
2019-07-03 22:17:40 -0500

    Aplicativo Corregido(encabezado datos)

--- Aplicativo.java
+++ Aplicativo.java
@@ -1,3 +1,12 @@
+/**********************************************
+* Nombre: Angulo Cortez Maria Jose            *
+* Carrera: 4to-Analisis de Sistemas Nocturno  *
+* Asignatura: Visual II                       *
+* Nom.Profesor: Fernado Solis                 *
+* Tema: Menu Banco                            *
+* Fecha: 03/07/2019                           *
+*                                             *
+**********************************************/
 public class Aplicativo{
 	public static void main(String []args){
 		Menu obj1=new Menu();
